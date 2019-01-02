91048a18203a685fa84195f1be373811ef7e2b76
YOONBYEONGIN <rpf5573@gmail.com>
2019-01-02 21:16:40 +0900

    minimize

--- src/user/user-client/components/bottom-navigation.js
+++ src/user/user-client/components/bottom-navigation.js
@@ -1,11 +1,11 @@
 import React, { Component, Fragment } from 'react';
 import { NavLink } from 'react-router-dom';
 import cn from 'classnames';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMap, faChartBar, faChessBoard, faImages, faInfo } from '@fortawesome/free-solid-svg-icons'
-
-library.add([faMap, faChartBar, faChessBoard, faImages, faInfo]);
+import MapIcon from '@material-ui/icons/Map';
+import ChartIcon from '@material-ui/icons/InsertChartOutlined';
+import PuzzleIcon from '@material-ui/icons/Dashboard';
+import UploadIcon from '@material-ui/icons/Camera';
+import PostInfoIcon from '@material-ui/icons/InfoSharp';
 
 class BottomNavigation extends Component {
   constructor(props) {
@@ -23,27 +23,27 @@ class BottomNavigation extends Component {
     return (
       <div className="bottom-navigation">
         <NavLink to="/user/page/map">
-          <div className="icon"><FontAwesomeIcon icon={faMap} /></div>
+          <div className="icon"><MapIcon /></div>
           <div className="text">지도</div>
           <div className={rippleCN}></div>
         </NavLink>
         <NavLink to="/user/page/point">
-          <div className="icon"><FontAwesomeIcon icon={faChartBar} /></div>
+          <div className="icon"><ChartIcon /></div>
           <div className="text">포인트</div>
           <div className={rippleCN}></div>
         </NavLink>
         <NavLink to="/user/page/puzzle">
-          <div className="icon"><FontAwesomeIcon icon={faChessBoard} /></div>
+          <div className="icon"><PuzzleIcon /></div>
           <div className="text">퍼즐</div>
           <div className={rippleCN}></div>
         </NavLink>
         <NavLink to="/user/page/upload">
-          <div className="icon"><FontAwesomeIcon icon={faImages} /></div>
+          <div className="icon"><UploadIcon /></div>
           <div className="text">업로드</div>
           <div className={rippleCN}></div>
         </NavLink>
         <NavLink to="/user/page/post-info">
-          <div className="icon"><FontAwesomeIcon icon={faInfo} /></div>
+          <div className="icon"><PostInfoIcon /></div>
           <div className="text">포스트</div>
           <div className={rippleCN}></div>
         </NavLink>
