0b686e144af716994dc462c1d5eddedd9595d09c
YOONBYEONGIN <rpf5573@gmail.com>
2019-01-03 16:26:33 +0900

    t

--- src/entrance/entranceRoute.js
+++ src/entrance/entranceRoute.js
@@ -3,6 +3,8 @@ const template = require('./entrance-client/template');
 
 module.exports = (app, DCQuery) => {
   app.get('/', (req, res) => {
+    console.log( 'HERE IS ' + process.env.NODE_PORT );
+
     let document = template(req.query.message);
     return res.set('Content-Type', 'text/html').end(document);
   });
