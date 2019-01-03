7728d7e8ffb039248ee3ae7b7c9505212a6642f2
YOONBYEONGIN <rpf5573@gmail.com>
2019-01-03 16:54:49 +0900

    delete watch

--- src/index.js
+++ src/index.js
@@ -49,7 +49,6 @@ require('./warehouse/init')(app, pool);
 
 app.use(express.static('public'));
 
-console.log( 'process.env.NODE_ENV : ', process.env.NODE_ENV );
 console.log( 'process.env.NODE_PORT : ', process.env.NODE_PORT );
 
 const PORT = process.env.NODE_PORT || 8081;
