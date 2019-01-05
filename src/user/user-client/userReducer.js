86cda53c5831a8c56f4b3b9e517144af8a32451e
YOONBYEONGIN <rpf5573@gmail.com>
2019-01-05 14:49:36 +0900

    t

--- src/user/user-client/userReducer.js
+++ src/user/user-client/userReducer.js
@@ -7,6 +7,7 @@ export default (state = {}, action) => {
         points: action.payload
       });
     
+      
     case UPDATE_PUZZLE_COLON_INFO:
       return Object.assign({}, state, {
         puzzleColonInfo: action.payload
